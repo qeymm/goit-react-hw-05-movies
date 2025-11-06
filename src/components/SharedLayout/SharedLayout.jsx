6938121223ce67df3f0e37f63342112b56618df8
Kim Enorme <kimenorme15@gmail.com>
2025-11-06 16:32:07 +0800

    added logo

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,18 +2,34 @@ import { Loader } from 'components/Loader/Loader';
 import { Suspense } from 'react';
 import { Outlet, NavLink } from 'react-router';
 import styles from './SharedLayout.module.css';
+import filmReel from 'assets/film-reel.svg';
 
 export const SharedLayout = () => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.bar}>
-          <div className={styles.brand}>CineScope</div>
+          <div className={styles.brand}>
+            FILM
+            <img src={filmReel} alt="film reel" className={styles.brandIcon} />
+            OD
+          </div>
           <nav className={styles.nav}>
-            <NavLink to="/" end className={({ isActive }) => isActive ? `${styles.link} ${styles.linkActive}` : styles.link}>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) =>
+                isActive ? `${styles.link} ${styles.linkActive}` : styles.link
+              }
+            >
               Home
             </NavLink>
-            <NavLink to="/movies" className={({ isActive }) => isActive ? `${styles.link} ${styles.linkActive}` : styles.link}>
+            <NavLink
+              to="/movies"
+              className={({ isActive }) =>
+                isActive ? `${styles.link} ${styles.linkActive}` : styles.link
+              }
+            >
               Movies
             </NavLink>
           </nav>
